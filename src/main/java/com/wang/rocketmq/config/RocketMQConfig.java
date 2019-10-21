3227b08ade066c2b0428cfb1293cde5ea5664794
wangfc <747379615@qq.com>
2019-10-21 23:08:58 +0800

    wang

--- src/main/java/com/wang/rocketmq/config/RocketMQConfig.java
+++ src/main/java/com/wang/rocketmq/config/RocketMQConfig.java
@@ -3,7 +3,7 @@ package com.wang.rocketmq.config;
 public class RocketMQConfig {
 
 
-    public static final String NAME_SERVER = "39.105.85.64:9876";
+    public static final String NAME_SERVER = "39.105.85.66:9876";
 //    public static final String NAME_SERVER = "192.168.199.211:9876;192.168.199.212:9876";
     public static final String PAY_GROUP = "pay_group";
     // 必须唯一
